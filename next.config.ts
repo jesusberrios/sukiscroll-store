ab03ca1102e83ed9e472892857b6c49579ae68f0
Jesus Berrios <jesus.berrios@haibu.cl>
2025-11-03 16:00:30 -0300

    first

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+const repoName = '/sukiscroll-store'; // nombre de tu repo en GitHub
+
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  output: 'export', // para next export
+  basePath: isProd ? repoName : '',
+  assetPrefix: isProd ? repoName + '/' : '',
   images: {
     remotePatterns: [
       {
